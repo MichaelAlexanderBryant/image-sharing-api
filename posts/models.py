3cce8d1c8a1be6e90858716a0ffab1f228e44d48
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-22 10:32:51 -0800

    Create API for list and detail views

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class Post(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='uploads/')
-    description = models.TextField(max_length=250)
+    caption = models.TextField(max_length=250)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
