91102c37a628094ea61e50a1d925387ff2bff781
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-24 09:08:30 -0800

    Add JWT authentication

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, MyTokenObtainPairView
 
 urlpatterns = [
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
     path("", PostList.as_view(), name="post_list")
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,12 @@ from rest_framework import generics, permissions
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, MyTokenObtainPairSerializer
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
