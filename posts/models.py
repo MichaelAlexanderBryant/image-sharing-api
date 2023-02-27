5855b3f083f7f4f7a74097edd4f7f7a32f242d07
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-27 15:21:27 -0800

    Add style to update image page

--- posts/models.py
+++ posts/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='static/')
     caption = models.TextField(max_length=250)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, to_field='username', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
